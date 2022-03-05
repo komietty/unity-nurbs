ce674571e83635f3bd9909ef6508c5ff0323d180
komietty <komietty@gmail.com>
2022-03-05 18:21:17 +0900

    update curvature viewer

--- Assets/Curvature/Curvature.cs
+++ Assets/Curvature/Curvature.cs
@@ -2,32 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-using UnityEngine.Assertions;
+using kmty.gist;
+using static UnityEngine.GraphicsBuffer;
 
 public class Curvature : MonoBehaviour {
     [SerializeField, Range(1, 10)] protected float colorScale;
-    GraphicsBuffer indexBuff;
-    GraphicsBuffer tableBuff;
-    GraphicsBuffer colorBuff;
-    GraphicsBuffer vertexBuff;
-    Material mat;
-
-    struct frto {
-        public int fr;
-        public int to1;
-        public int to2;
-        public frto(int fr, int to1, int to2) {
-            this.fr = fr;
-            this.to1 = to1;
-            this.to2 = to2;
-        }
-    }
-
-    struct bgln {
-        public int bg;
-        public int ln;
-        public bgln(int bg, int ln) { this.bg = bg; this.ln = ln; }
-    }
+    [SerializeField] protected ComputeShader cs;
+    [SerializeField] protected bool weld;
+    protected int len = 0;
+    protected Material mat;
+    protected GraphicsBuffer tblBuff, colBuff, idxBuff, vrtBuff;
 
     Mesh Weld(Mesh original) {
         var ogl_vrts = original.vertices;
@@ -54,101 +38,70 @@ public class Curvature : MonoBehaviour {
 
     void Start() {
         var filt = GetComponent<MeshFilter>();
-        var mesh = Weld(filt.sharedMesh);
-        var vrts = mesh.vertices.Select(v => v * 10000).ToArray();
+        var mesh = filt.sharedMesh;
+        if (weld) {
+            mesh = Weld(filt.sharedMesh);
+            filt.mesh = mesh;
+        }
+        var vrts = mesh.vertices;
         var tris = mesh.GetIndices(0);
-        var frtos = new frto[tris.Length];
-        var bglns = new bgln[vrts.Length];
-        filt.mesh = mesh;
+        var frtos = new Vector3Int[tris.Length];
+        var bglns = new Vector2Int[vrts.Length];
         mat = GetComponent<MeshRenderer>().sharedMaterial;
-
-        Debug.Log(vrts.Length);
-        Debug.Log(tris.Length);
-        //foreach (var v in vrts) { Debug.Log(v); }
+        len = vrts.Length;
 
         for (var i = 0; i < tris.Length; i += 3) {
             var i0 = tris[i];
             var i1 = tris[i + 1];
             var i2 = tris[i + 2];
-            frtos[i + 0] = new frto(i0, i1, i2);
-            frtos[i + 1] = new frto(i1, i2, i0);
-            frtos[i + 2] = new frto(i2, i0, i1);
+            frtos[i + 0] = new Vector3Int(i0, i1, i2);
+            frtos[i + 1] = new Vector3Int(i1, i2, i0);
+            frtos[i + 2] = new Vector3Int(i2, i0, i1);
         }
 
-        frtos = frtos.OrderBy(ft => ft.fr).ToArray();
+        frtos = frtos.OrderBy(ft => ft.x).ToArray();
 
-        var fr = 0;
-        var bg = 0;
+        int fr = 0;
+        int bg = 0;
         for(var i = 0; i < frtos.Length; i++){
             var curr = frtos[i];
-            if (curr.fr == fr + 1){
-                bglns[fr] = new bgln(bg, i - bg);
+            if (curr.x == fr + 1){
+                bglns[fr] = new Vector2Int(bg, i - bg);
                 bg = i;
                 fr++;
-                if(curr.fr == vrts.Length - 1){
-                    bglns[curr.fr] = new bgln(bg, frtos.Length - bg);
+                if (curr.x == vrts.Length - 1) {
+                    bglns[curr.x] = new Vector2Int(bg, frtos.Length - bg);
                 }
             }
         }
 
-        //test
-        //foreach(var f in frtos){ Debug.Log("fr: " + f.fr + ", to1: " + f.to1 + ", to2: " + f.to2); }
-        //foreach(var bl in bglns){ Debug.Log("bg: " + bl.bg + ", ln: " + bl.ln); }
-        //Assert.IsTrue(bglns.Length == vrts.Length);
-        var cols = new float[vrts.Length];
-        for (var vid = 0; vid < vrts.Length; vid++) {
-            var vrt = vrts[vid];
-            var bgn = bglns[vid].bg;
-            var len = bglns[vid].ln;
-            var sum = 0f;
-            for(var i = 0; i < len; i++){
-                var frid = frtos[i + bgn].fr;
-                var t1id = frtos[i + bgn].to1;
-                var t2id = frtos[i + bgn].to2;
-                var p0 = vrts[frid];
-                var p1 = vrts[t1id];
-                var p2 = vrts[t2id];
-                var l1 = (p1 - p0).magnitude;
-                var l2 = (p2 - p0).magnitude;
-                if(l1 > 0 && l2 > 0){
-                    var d1 = (p1 - p0) / l1;
-                    var d2 = (p2 - p0) / l2;
-                    var rad = Mathf.Acos(Vector3.Dot(d1, d2));
-                    sum += rad;
-                }
-            }
-            var k = (1f - sum / (Mathf.PI * 2)) * 12;
-            cols[vid] = k;
-            //Debug.Log( Mathf.FloorToInt(k * 10000) * 0.0001f);
-        }
-
-        //test
-
-        indexBuff = new GraphicsBuffer(GraphicsBuffer.Target.Structured, frtos.Length, sizeof(int) * 3);
-        tableBuff = new GraphicsBuffer(GraphicsBuffer.Target.Structured, bglns.Length, sizeof(int) * 2);
-        indexBuff.SetData(frtos);
-        tableBuff.SetData(bglns);
-
-        vertexBuff = new GraphicsBuffer(GraphicsBuffer.Target.Structured, vrts.Length, sizeof(float) * 3);
-        vertexBuff.SetData(vrts);
-
-        colorBuff = new GraphicsBuffer(GraphicsBuffer.Target.Structured, cols.Length, sizeof(float));
-        colorBuff.SetData(cols);
-
-        mat.SetBuffer("indexBuff", indexBuff);
-        mat.SetBuffer("tableBuff", tableBuff);
-        mat.SetBuffer("colorBuff", colorBuff);
-        mat.SetBuffer("vertexBuff", vertexBuff);
+        vrtBuff = new GraphicsBuffer(Target.Vertex, vrts.Length, sizeof(float) * 3);
+        colBuff = new GraphicsBuffer(Target.Structured, vrts.Length, sizeof(float));
+        idxBuff = new GraphicsBuffer(Target.Structured, frtos.Length, sizeof(int) * 3);
+        tblBuff = new GraphicsBuffer(Target.Structured, bglns.Length, sizeof(int) * 2);
+        idxBuff.SetData(frtos);
+        tblBuff.SetData(bglns);
+        vrtBuff.SetData(vrts);
     }
 
-    void OnDestroy(){
-        indexBuff.Dispose();
-        tableBuff.Dispose();
-        colorBuff.Dispose();
-        vertexBuff.Dispose();
+    void Update() {
+        var k = cs.FindKernel("CalcCurvature");
+        cs.SetBuffer(k, "_Vrts", vrtBuff);
+        cs.SetBuffer(k, "_Idxs", idxBuff);
+        cs.SetBuffer(k, "_Table", tblBuff);
+        cs.SetBuffer(k, "_Curvatures", colBuff);
+        ComputeShaderUtil.Dispatch1D(cs, k, len);
     }
 
     void OnRenderObject() {
         mat.SetFloat("_ColorScale", colorScale);
-     }
+        mat.SetBuffer("_Curvature", colBuff);
+    }
+
+    void OnDestroy() {
+        idxBuff.Dispose();
+        tblBuff.Dispose();
+        colBuff.Dispose();
+        vrtBuff.Dispose();
+    }
 }
--- Assets/Curvature/Kd.shader
+++ Assets/Curvature/Kd.shader
@@ -10,7 +10,6 @@ Shader "Unlit/Kd" {
             #pragma vertex vert
             #pragma fragment frag
             #include "UnityCG.cginc"
-            #define PI 3.1415926538
 
             struct appdata {
                 float4 vertex : POSITION;
@@ -21,56 +20,17 @@ Shader "Unlit/Kd" {
                 float4 color: COLOR;
             };
 
-            struct FrTo {
-                int fr;
-                int to1;
-                int to2;
-            };
-
-            struct BgLn {
-                int bg;
-                int ln;
-            };
-
-            StructuredBuffer<FrTo> indexBuff;
-            StructuredBuffer<BgLn> tableBuff;
-            StructuredBuffer<float3> vertexBuff;
-            StructuredBuffer<float> colorBuff;
-
-
             sampler2D _MainTex;
             float4 _MainTex_ST;
             float _ColorScale;
+            StructuredBuffer<float> _Curvature;
 
             v2f vert (uint vid: SV_VertexID, appdata v) {
                 v2f o;
-                //BgLn bgln = tableBuff[vid];
-                //int bg = bgln.bg;
-                //int ln = bgln.ln;
-                //float sum = 0.0;
-                //for(int i = 0; i < ln; i++){
-                //    int fr  =  indexBuff[bg + i].fr;
-                //    int to1 =  indexBuff[bg + i].to1;
-                //    int to2 =  indexBuff[bg + i].to2;
-                //    float3 p0 = vertexBuff[fr];
-                //    float3 p1 = vertexBuff[to1];
-                //    float3 p2 = vertexBuff[to2];
-                //    float3 d1 = p1 - p0;
-                //    float3 d2 = p2 - p0;
-                //    float l1 = length(d1);
-                //    float l2 = length(d2);
-                //    if(l1 > 0 && l2 > 0){
-                //        float3 n1 = d1 / l1;
-                //        float3 n2 = d2 / l2;
-                //        float rad = acos(dot(n1, n2));
-                //        sum += rad;
-                //    }
-                //}
-                //float k = (1.0 - sum) / (PI * 2) * 12 * _ColorScale;
-
-                //o.color = float4(1.0, 0.0, 0.0, 1.0);
-                float k = colorBuff[vid] * _ColorScale;
-                o.color = float4(clamp(k, 0, 1), 0, clamp(-k, 0, 1), 1);
+                float k = _Curvature[vid] * _ColorScale;
+                float e = clamp(k, 0, 1);
+                float s = clamp(-k, 0, 1);
+                o.color = float4(1 - s, 1 - s - e, 1 - e, 1);
                 o.vertex = UnityObjectToClipPos(v.vertex);
                 return o;
             }
